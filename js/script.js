110978fda4ebe181b482832f2b23a2556e5d3242
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-01-15 18:41:15 +0530

    feat: setup basic UI

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@
  */
 
 $(document).ready(function () {
+    $('.data')
+        .hide()  // Hide it initially
+        .ajaxStart(function () {
+            $(this).show();
+            $("#placeholder").html("Fetching latest results...");
+        });
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             var lat = position.coords.latitude;
@@ -13,16 +19,38 @@ $(document).ready(function () {
                 function (json) {
                     var description = json.weather[0].description;
                     var temp = json.main.temp;
-                    var pressure = json.main.pressure;
                     var humidity = json.main.humidity;
-                    var temp_min = json.main.temp_min;
-                    var temp_max = json.main.temp_max;
-                    var wind_speed = json.wind.speed;
-                    var wind_direction = json.wind.deg;
-                    $("#test").html(description + " " + temp + " " + wind_direction + " " + wind_speed + " " + humidity);
+                    var clouds = json.clouds.all;
+
+                    $("#temp").html(temp);
+                    $("#humidity").html(humidity);
+                    $("#description").html(description);
+                    var intensity = 1;
+                    if (clouds <= 5) {
+                        intensity = 0;
+                    } else if (clouds > 5 && clouds < 20) {
+                        intensity = 0.2;
+                    } else if (clouds >= 20 && clouds < 40) {
+                        intensity = 0.4;
+                    } else if (clouds >= 40 && clouds < 60) {
+                        intensity = 0.6;
+                    } else if (clouds >= 60 && clouds < 80) {
+                        intensity = 0.8;
+                    } else if (clouds >= 80 && clouds <= 100) {
+                        intensity = 1;
+                    }
+                    $(".cloud_data").css({opacity: intensity});
+
+                    $(".data")
+                        .show()
+                        .ajaxStop(function () {
+                            $(this).hide();
+                            $("#placeholder").html(" ");
+                        });
                 });
         });
     } else {
         $("#test").html("Couldn't get lat long");
     }
+
 });
